f79548a8d8d84e5efc0a230320b574d7e59e681a
ValeeFernandez <valeriafernandez1715@gmail.com>
2025-04-05 12:22:59 -0600

    Home Desing

---
+++ frontend/pokeProjectFE/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from 'react'; 
+import {
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+} from '@ionic/react';
+import './Sidebar.css';
+
+const Sidebar: React.FC = () => {
+  return (
+    <IonMenu side="start" menuId="main-menu" contentId="main-content">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle className='pokemon-title'>Menú</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='pokemon-content'>
+        <IonList className='pokemon-list'>
+          <IonItem button className="pokemon-button">
+            <span className="pokemon-arrow">▶ </span>
+            <IonLabel>Poke Búsqueda</IonLabel>
+          </IonItem>
+          <IonItem button className="pokemon-button">
+            <span className="pokemon-arrow">▶ </span>
+            <IonLabel>Comparar Pokémons</IonLabel>
+          </IonItem>
+          <IonItem button className="pokemon-button">
+            <span className="pokemon-arrow">▶ </span>
+            <IonLabel>Favoritos</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Sidebar;
--- frontend/pokeProjectFE/src/pages/Home.tsx
+++ frontend/pokeProjectFE/src/pages/Home.tsx
@@ -1,24 +1,74 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from 'react';
+import {
+  IonApp,
+  IonFooter,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  IonMenuToggle,
+  IonButton,
+  IonButtons,
+} from '@ionic/react';
+import Sidebar from '../components/Sidebar/Sidebar';
+import { colorFill, menuOutline } from 'ionicons/icons';
+import { IonIcon } from '@ionic/react';
+
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <IonApp>
+      <Sidebar /> {/* Importamos el menú lateral */}
+
+      <IonPage id="main-content">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonButtons slot="start">
+              <IonMenuToggle menu="main-menu">
+                <IonButton fill="clear">
+                  <IonIcon icon={menuOutline} slot="start"
+                    className="menu-icon" style={{ fontSize: '50px', color: '#4A7C99', margin: '10px' }} />
+                </IonButton>
+              </IonMenuToggle>
+            </IonButtons>
+
+            <div style={{ position: 'relative', left: 0, right: 15, textAlign: 'center', pointerEvents: 'none', marginTop: '10px' }}>
+              <IonTitle className='pokemon-title' style={{ color: '#4A7C99', display: 'inline-block', pointerEvents: 'auto', fontSize: '3rem' }}>
+                PokeLab
+              </IonTitle>
+            </div>
+
+            <IonButtons slot="end">
+              <img src="public/assets/pokemon.png" alt="Pokémon Logo"
+                style={{ height: '60px', marginRight: '20px', margin: '10px' }} />
+            </IonButtons>
           </IonToolbar>
+
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+
+        <IonContent fullscreen>
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              minWidth: '100%',
+              minHeight: '100%',
+              objectFit: 'cover',
+              zIndex: -1,
+            }}
+          >
+            <source src="public/assets/video2.mp4" type="video/mp4" />
+            Tu navegador no soporta el video.
+          </video>
+        </IonContent>
+      </IonPage>
+    </IonApp>
   );
 };
 
