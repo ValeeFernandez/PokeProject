9497bb2ed63df87a0dc59c7dcd11945603b1f888
Randall_Wa <randall.alvarez.ch@gmail.com>
2025-04-06 10:39:05 -0600

    NotFound added and icon

--- frontend/pokeProjectFE/src/App.tsx
+++ frontend/pokeProjectFE/src/App.tsx
@@ -1,7 +1,11 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+
+/* IMPORTACIONES DE LAS PAG */
 import Home from './pages/Home';
+import NotFound from './pages/NotFound/NotFound';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -43,6 +47,9 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
+        </Route>
+        <Route>
+          <NotFound />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
---
+++ frontend/pokeProjectFE/src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './NotFound.css';
+import { IonButton } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+
+import Home from '../Home';
+
+const NotFound: React.FC = () => {
+  const history = useHistory();
+
+  return (
+    <div className="notfound-container">
+      <img src="/assets/pikachuError.jpg" alt="Pikachu Error" className="notfound-img" />
+      <div className="notfound-text">
+        <h1 className="notfound-title">404</h1>
+        <p className="notfound-subtitle">Lo siento, no encontramos esa página jiji.</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
